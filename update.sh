503ca519b2fcae051d0d8c20678d7112b5351d18
z0890142 <a0955117097@gmail.com>
2020-05-30 21:16:09 +0800

    modify script

--- update.sh
+++ update.sh
@@ -3,73 +3,74 @@ DB_USER='developer';
 DB_PASSWD='1qaz@WSX3edc';
 
 mysql --user=$DB_USER --password=$DB_PASSWD << EOF
+use ForeignCurrency
 
 update ForeignCurrencyRecord as fr inner join
 (
-    SELECT fsp.SN,Date,15_Heighest FROM ForeignCurrencyBuyInPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
-    and Price=fr.15_Heighest
+    SELECT fsp.SN,Date,15_Heigest FROM ForeignCurrencyBuyInPrice as fsp
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    and Price=fr.15_Heigest
 ) as f
 on fr.SN=f.SN
-set fr.15_Heighest_Date =f.Date;
+set fr.15_Heigest_Date =f.Date;
 
 update ForeignCurrencyRecord as fr inner join
 (
-    SELECT fsp.SN,Date,3Month_Heighest FROM ForeignCurrencyBuyInPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
-    and Price=fr.3Month
+    SELECT fsp.SN,Date,3Month_Heigest FROM ForeignCurrencyBuyInPrice as fsp
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    and Price=fr.3Month_Heigest
 ) as f
 on fr.SN=f.SN
-set fr.3Month_Date =f.Date;
+set fr.3Month_Heigest_Date =f.Date;
 
 update ForeignCurrencyRecord as fr inner join
 (
-    SELECT fsp.SN,Date,6Month_Heighest FROM ForeignCurrencyBuyInPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
-    and Price=fr.6Month
+    SELECT fsp.SN,Date,6Month_Heigest FROM ForeignCurrencyBuyInPrice as fsp
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    and Price=fr.6Month_Heigest
 ) as f
 on fr.SN=f.SN
-set fr.6Month_Date =f.Date;
+set fr.6Month_Heigest_Date =f.Date;
 
 update ForeignCurrencyRecord as fr inner join
 (
-    SELECT fsp.SN,Date,1Year_Heighest FROM ForeignCurrencyBuyInPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
-    and Price=fr.1Year
+    SELECT fsp.SN,Date,1Year_Heigest FROM ForeignCurrencyBuyInPrice as fsp
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    and Price=fr.1Year_Heigest
 ) as f
 on fr.SN=f.SN
-set fr.1Year_Date =f.Date;
+set fr.1Year_Heigest_Date =f.Date;
 
 update ForeignCurrencyRecord as fr inner join
 (
-    SELECT fsp.SN,Date,3Year_Heighest FROM ForeignCurrencyBuyInPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
-    and Price=fr.3Year
+    SELECT fsp.SN,Date,3Year_Heigest FROM ForeignCurrencyBuyInPrice as fsp
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    and Price=fr.3Year_Heigest
 ) as f
 on fr.SN=f.SN
-set fr.3Year_Date =f.Date;
+set fr.3Year_Heigest_Date =f.Date;
 
 update ForeignCurrencyRecord as fr inner join
 (
-    SELECT fsp.SN,Date,5Year_Heighest FROM ForeignCurrencyBuyInPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
-    and Price=fr.5Year
+    SELECT fsp.SN,Date,5Year_Heigest FROM ForeignCurrencyBuyInPrice as fsp
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    and Price=fr.5Year_Heigest
 ) as f
 on fr.SN=f.SN
-set fr.5Year_Date =f.Date;
+set fr.5Year_Heigest_Date =f.Date;
 
 
 update ForeignCurrencyRecord as fr inner join
 (
     SELECT fsp.SN,Date,15_Lowest FROM ForeignCurrencySellPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
     and Price=fr.15_Lowest
 ) as f
 on fr.SN=f.SN
@@ -79,8 +80,8 @@ set fr.15_Lowest_Date =f.Date;
 update ForeignCurrencyRecord as fr inner join
 (
     SELECT fsp.SN,Date,3Month_Lowest_Date FROM ForeignCurrencySellPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
     and Price=fr.3Month_Lowest
 ) as f
 on fr.SN=f.SN
@@ -89,8 +90,8 @@ set fr.3Month_Lowest_Date =f.Date;
 update ForeignCurrencyRecord as fr inner join
 (
     SELECT fsp.SN,Date,6Month_Lowest_Date FROM ForeignCurrencySellPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
     and Price=fr.6Month_Lowest
 ) as f
 on fr.SN=f.SN
@@ -99,8 +100,8 @@ set fr.6Month_Lowest_Date =f.Date;
 update ForeignCurrencyRecord as fr inner join
 (
     SELECT fsp.SN,Date,1Year_Lowest_Date FROM ForeignCurrencySellPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
     and Price=fr.1Year_Lowest
 ) as f
 on fr.SN=f.SN
@@ -109,8 +110,8 @@ set fr.1Year_Lowest_Date =f.Date;
 update ForeignCurrencyRecord as fr inner join
 (
     SELECT fsp.SN,Date,3Year_Lowest_Date FROM ForeignCurrencySellPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
     and Price=fr.3Year_Lowest
 ) as f
 on fr.SN=f.SN
@@ -119,8 +120,8 @@ set fr.3Year_Lowest_Date =f.Date;
 update ForeignCurrencyRecord as fr inner join
 (
     SELECT fsp.SN,Date,5Year_Lowest_Date FROM ForeignCurrencySellPrice as fsp
-    join ForeignCurrencyRecord fr on fsp.SN=fr.SN
-    where Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
+    join ForeignCurrencyRecord as fr on fsp.SN=fr.SN
+    where fsp.Date between DATE_SUB(CURDATE(), INTERVAL 3 Year) and CURDATE() 
     and Price=fr.5Year_Lowest
 ) as f
 on fr.SN=f.SN
